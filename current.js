fe56a44c7573916de9f2e8dd0906cd8eb559526f
Your Name <daniel.stine@me.com>
2018-10-22 14:01:59 -0500

    Introduced div structure for html pages, added api call functions to current.js - added ip address data to the location information for AccuWeather API access

--- current.js
+++ current.js
@@ -7,6 +7,22 @@ function Location(latitude, longitude, city, state) {
     this.state = state;
 }
 
+function CurrentConditions(currentSummary, nearestStormDistance,
+        precipProbability, precipType, temperature,
+        apparentTemperature, dewPoint, humidity, windSpeed,
+        windBearing) {
+    this.currentSummary = currentSummary;
+    this.nearestStormDistance = nearestStormDistance + " miles";
+    this.precipProbability = (Math.round(precipProbability * 100)) + "%";
+    this.precipType = precipType;
+    this.temperature = (Math.round(temperature)) + "°";
+    this.apparentTemperature = (Math.round(apparentTemperature)) + "°";
+    this.dewPoint = (Math.round(dewPoint)) + "°";
+    this.humidity = (Math.round(humidity * 100)) + "%";
+    this.windSpeed = (Math.round(windSpeed)) + " mph";
+    this.windBearing = bearing(windBearing);
+}
+
 function getLocation() {
     let location;
     if (window.sessionStorage.length == 0) {
@@ -46,6 +62,84 @@ function retrieveLocation() {
     return location;
 }
 
-document.addEventListener("DOMContentLoaded", event => {
+function getConditions(location) {
+    let conditions;
+    let apiKey = "f386c5d5bd0791645dc039c3dc85e08f";
+    let proxy = "https://cors-anywhere.herokuapp.com/"
+    let forecastURL = `https://api.darksky.net/forecast/${apiKey}/${location.latitude},${location.longitude}`;
+    fetch(proxy + forecastURL)
+        .then(res => res.json())
+        .then(data => {
+            conditions = new CurrentConditions(data.currently.summary,
+                data.currently.nearestStormDistance,
+                data.currently.precipProbability,
+                data.currently.precipType,
+                data.currently.temperature,
+                data.currently.apparentTemperature,
+                data.currently.dewPoint,
+                data.currently.humidity,
+                data.currently.windSpeed,
+                data.currently.windBearing);
+                refreshDisplay(conditions);
+        })
+        .catch(err => console.log(err));
+}
 
+function refreshDisplay(conditions) {
+    let forecastDisplay = document.getElementsByClassName("forecast-display")[0].firstElementChild;
+    let newline = "<br>";
+    forecastDisplay.innerHTML = "The current weather: " + conditions.currentSummary
+         + newline + "Nearest Storm: " + conditions.nearestStormDistance +
+         "\t Probability of precipitation: " + conditions.precipProbability +
+         newline + "Precipitation type: " + conditions.precipType + newline +
+         "Temperature: " + conditions.temperature + "\t Feels like: " +
+          conditions.apparentTemperature + newline +
+         "Dew point: " + conditions.dewPoint + "\t Humidity: " +
+         conditions.humidity + newline + "Wind speed: " +
+         conditions.windSpeed + " " + conditions.windBearing; 
+}
+
+function bearing(windBearing) {
+    let ordinalValue = Math.round(windBearing / 16);
+    switch (ordinalValue) {
+        case 0:
+            return "N";
+        case 1:
+            return "NNE";
+        case 2:
+            return "NE";
+        case 3:
+            return "ENE";
+        case 4:
+            return "E";
+        case 5:
+            return "ESE";
+        case 6:
+            return "SE";
+        case 7:
+            return "SSE";
+        case 8:
+            return "S";
+        case 9:
+            return "SSW";
+        case 10:
+            return "SW";
+        case 11:
+            return "WSW";
+        case 12:
+            return "W";
+        case 13:
+            return "WNW";
+        case 14:
+            return "NW";
+        case 15:
+            return "NNW";
+        default:
+            return "N";
+   }
+}
+
+document.addEventListener("DOMContentLoaded", event => {
+    let location = getLocation();
+    getConditions(location);
 });
